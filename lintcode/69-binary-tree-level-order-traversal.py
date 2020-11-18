4cae1ae298f919116e1bb1c0eb178785e8ad3d56
imJeffZ <jefferson.zhong@mail.utoronto.ca>
2020-11-18 15:30:36 -0800

    Finishe 69

--- lintcode/69-binary-tree-level-order-traversal.py
+++ lintcode/69-binary-tree-level-order-traversal.py
@@ -1,10 +1,12 @@
+from collections import deque
+
 """
 Definition of TreeNode:
+"""
 class TreeNode:
     def __init__(self, val):
         self.val = val
         self.left, self.right = None, None
-"""
 
 class Solution:
     """
@@ -13,3 +15,18 @@ class Solution:
     """
     def levelOrder(self, root):
         # write your code here
+        if not root:
+            return []
+        queue = deque([root])
+        res = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
