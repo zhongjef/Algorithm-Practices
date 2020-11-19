5d0751d71b8e389dd11e5a91b851cf3d24bfb4b5
imJeffZ <jefferson.zhong@mail.utoronto.ca>
2020-11-18 18:03:31 -0800

    Finish 1360

---
+++ lintcode/1360-symmetric-tree.py
@@ -0,0 +1,29 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: root of the given tree
+    @return: whether it is a mirror of itself 
+    """
+    def isSymmetricST(self, left, right):
+        if left and not right:
+            return False
+        if right and not left:
+            return False
+        if not left and not right:
+            return True
+        if left.val != right.val:
+            return False
+        return self.isSymmetricST(left.left, right.right) and self.isSymmetricST(left.right, right.left)
+    
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isSymmetricST(root.left, root.right)
+        
