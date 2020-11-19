69741c69c5e67ee83908141e6c58ebda77e0d5e6
imJeffZ <jefferson.zhong@mail.utoronto.ca>
2020-11-18 17:30:28 -0800

    Finish 112

---
+++ lintcode/112-remove-duplicates-from-sorted-list.py
@@ -0,0 +1,24 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: head is the head of the linked list
+    @return: head of linked list
+    """
+    def deleteDuplicates(self, head):
+        # write your code here
+        if not head:
+            return
+        curr = head
+        while curr.next:
+            if curr.next.val == curr.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
